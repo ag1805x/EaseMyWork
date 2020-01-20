6f8b6b2aa57a0973f1aec09db9e5c499bb03bd0b
Arindam Ghosh <ag1805x@outlook.in>
2020-01-20 10:40:14 +0530

    Update ComputerInfo.sh

--- ComputerInfo.sh
+++ ComputerInfo.sh
@@ -1,2 +1,8 @@
-# Commands to check basic computer information
+# Commands to check basic computer information in Ubuntu
+
+# RAM hardware info
+sudo dmidecode --type memory
+
+# RAM usage
+free -h
 
